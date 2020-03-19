24c7ecd762a336645aa023684b5487deadcb791e
Leonardo Oteca <lfotecam@gmail.com>
2020-03-19 10:44:20 -0500

    Project setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 class App extends React.Component {
   render() {
     const { name } = this.props;
-    return <h1>Hello {name}</h1>;
+    return <h1 className="container">Hello {name}</h1>;
   }
 }
 
